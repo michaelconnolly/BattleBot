0c32b302663850ef608447edb5f7900e0bdf88d0
Brian <brian@connolly.net>
2020-02-15 12:16:44 -0800

    Update battlebot.ino
    - memory mangement
    - detect bluetooth adress at startup, and display on console.

--- arduino/battlebot/battlebot.ino
+++ arduino/battlebot/battlebot.ino
@@ -1,27 +1,31 @@
-    // Import external libraries
+// Import external libraries
 #include <Adafruit_ssd1306syp.h>
 #include "SoftwareSerial.h"
-//#include "WString.h"
 #include <Servo.h>
 
 // Constants: I/O Pins
-#define PIN_BLUETOOTH_RECV 2
-#define PIN_BLUETOOTH_SEND 3
-#define PIN_MOTOR_A_ENABLE 5
-#define PIN_MOTOR_B_ENABLE 6
-#define PIN_MOTOR_A_INPUT1 7
-#define PIN_MOTOR_A_INPUT2 8
-#define PIN_MOTOR_B_INPUT1 11
-#define PIN_MOTOR_B_INPUT2 4
-#define PIN_LED            10
-#define PIN_I2C_SDA        A4
-#define PIN_I2C_SCL        A5
-#define PIN_SERVO          9
+#define PIN_BLUETOOTH_RECV    2
+#define PIN_BLUETOOTH_SEND    3
+#define PIN_BLUETOOTH_POWER   12
+#define PIN_BLUETOOTH_ENABLE  13
+#define PIN_MOTOR_A_ENABLE    5
+#define PIN_MOTOR_B_ENABLE    6
+#define PIN_MOTOR_A_INPUT1    7
+#define PIN_MOTOR_A_INPUT2    8
+#define PIN_MOTOR_B_INPUT1    11
+#define PIN_MOTOR_B_INPUT2    4
+#define PIN_LED               10
+#define PIN_I2C_SDA           A4
+#define PIN_I2C_SCL           A5
+#define PIN_SERVO             9
 
 // More Constants
 #define AUTO_SHUTOFF_TIME 30000
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define BLUETOOTH_DETECTION_TIME 8000
+#define BLUETOOTH_READ_BUFFER 30
 
 // The various states of our bluetooth connection.
 enum BluetoothState {
@@ -33,9 +37,9 @@ enum BluetoothState {
 
 // Global Variables: Run state
 boolean dead = false;
-bool autoShutOff = false;
+boolean autoShutOff = false;
 unsigned long startTime = 0; 
-const char build_timestamp[] =  __DATE__ " " __TIME__;
+const char buildTimestamp[] =  __DATE__ " " __TIME__;
 
 // Global Variables: the OLED Display, connected via I2C interface
 Adafruit_ssd1306syp display(PIN_I2C_SDA, PIN_I2C_SCL);
@@ -46,77 +50,17 @@ int velocity = 100;
 // Global Variables: Bluetooth command Queue
 SoftwareSerial bluetooth(PIN_BLUETOOTH_RECV, PIN_BLUETOOTH_SEND);
 BluetoothState bluetoothState = BLUETOOTH_DISCONNECTED;
+char bluetoothReadbuffer[BLUETOOTH_READ_BUFFER + 1];
+int bluetoothAddressState = 0;
+char bluetoothAddress[15];
+
 char command = 'S';
 char prevCommand = 'A';
 unsigned long timeLastCommand = 0;  //Stores the time when the last command was received from the phone
 
 // Global Variables: Servo Weapon
 Servo servoMotor; 
-float servoPos = 0.0; 
-
-/*** DISPLAY ***/
-
-
-/**
- * Main output for status while in main sequence. This is called once per loop.
- */
-void displayStatus(String line1, String line2, String line3, String line4) {  
-  display.clear();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-
-  display.setCursor(0,2);
-  display.println(line1);
-
-  // Fun little animation to prove that we are not locked up.
-  const int circleRadius = 5;
-  const int circleOffset = 0;
-  const int maxRight = (SCREEN_WIDTH - 1) - circleRadius;
-  const int maxLeft = 82;
-  int numFrames = maxRight - maxLeft;
-  int numFramesDouble = numFrames * 2;
-  int timePerFrame = 3000 / numFramesDouble;
-  int currentFrame = (millis() / timePerFrame) % numFramesDouble;
-  int circleCenterX = (currentFrame < numFrames) ? (maxRight - currentFrame) : (maxLeft + (currentFrame - numFrames));
-  display.drawCircle(circleCenterX, circleRadius + circleOffset, circleRadius, WHITE);
-  
-  display.setCursor(0,20);
-  display.println(line2);
-    
-  display.setCursor(0,30);
-  display.println(line3);
-
-  display.setCursor(0,40);
-  display.println(line4);
-
-  // Print the timestamp of when we built this code.
-  const int stampHeight = 10;
-  int beginStamp = 55;
-  display.drawLine(0, beginStamp, SCREEN_WIDTH - 1, beginStamp, WHITE); // top line
-  display.drawLine(0, beginStamp, 0, beginStamp + stampHeight, WHITE); // left line
-  display.drawLine(SCREEN_WIDTH - 1, beginStamp, SCREEN_WIDTH - 1, beginStamp + stampHeight, WHITE); // right line
-  display.setCursor(4, beginStamp + 2);
-  display.println(build_timestamp);
-
-  display.update();
-}
-
-/**
- * Used for error messages to the OLED display.
- */
-void displayMessage(String line1, String line2) {
-  display.clear();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  
-  display.setCursor(0,2);
-  display.println(line1);
-  
-  display.setCursor(0,34);
-  display.println(line2);
-  
-  display.update();
-}
+float servoPos = 0.0;
 
 
 /**
@@ -131,7 +75,7 @@ void setup() {
   // the robot into the USB port, and get real time debug messages.
   // Make sure you set the baudrate at 9600 in Serial Monitor as well.
   Serial.begin(9600);
-  Serial.println("setup start...");
+  Serial.println(F("setup start..."));
 
   // Init motor control pins.
   pinMode(PIN_MOTOR_A_INPUT1, OUTPUT);
@@ -140,27 +84,37 @@ void setup() {
   pinMode(PIN_MOTOR_B_INPUT2, OUTPUT);
   pinMode(PIN_MOTOR_A_ENABLE, OUTPUT);
   pinMode(PIN_MOTOR_B_ENABLE, OUTPUT);
+  Serial.println(F("Motor setup complete..."));
 
   // Init LED pin, and initially set it to off.
   pinMode(PIN_LED, OUTPUT);
   digitalWrite(PIN_LED, LOW);
+  Serial.println(F("LED setup complete..."));
 
-  // Init the serial pipe to the bluetooth receiver. NOTE: this can go faster by configuring the HC-05
-  // with an AT command. But is that necessary?
-  bluetooth.begin(9600);
+  // Init the serial pipe to the bluetooth receiver
+  pinMode(PIN_BLUETOOTH_POWER, OUTPUT);
+  pinMode(PIN_BLUETOOTH_ENABLE, OUTPUT);
+  digitalWrite(PIN_BLUETOOTH_POWER, LOW);
+  digitalWrite(PIN_BLUETOOTH_ENABLE, LOW);
+  bluetoothAddressState = 0;
+  strcpy(bluetoothAddress, "unknown");
+  bluetooth.begin(38400);
+  Serial.println(F("Bluetooth setup complete..."));
   
   // Init the OLED display.
   delay(1000);
   display.initialize();
+  Serial.println(F("OLED setup complete..."));
 
   // Servo eapon
   servoMotor.attach(PIN_SERVO); 
   servoPos = 0.0;
   servoMotor.write(0.0);
+  Serial.println(F("Servo setup complete..."));
 
   // Init the rest of our internal state.
   dead = false;
-  Serial.println("setup end");
+  Serial.println(F("setup end"));
 }
 
 
@@ -168,12 +122,35 @@ void setup() {
  * Main Loop: called over and over again as the robot runs, 
  */
 void loop() {
+
+  // Get the current time.
+  int now = millis();
   
   // If we have marked as dead, be dead and do nothing.
   if (dead) { 
-    displayMessage("DEAD", "Deactivated due to auto-shutoff");
+    //displayMessage("DEAD", "Deactivated due to auto-shutoff");
     return;
   }
+
+  // Have we determined our bluetooth address yet?
+  if (!bluetoothAddressState && ((now - startTime) > BLUETOOTH_DETECTION_TIME)) { 
+    switchToBluetoothCommandMode();
+    writeToBluetoothCommand("AT+ADDR?");
+    //writeToBluetoothCommand("AT");
+
+    const char* response = readFromBluetoothCommand();
+    if (strlen(response) == 20) {
+      // +ADDR:98d3:b1:fd60df
+      strncpy(bluetoothAddress, response + 6, 14);
+    } else {
+      strcpy(bluetoothAddress, "invalid");
+    } 
+
+    switchToBluetoothNormalMode();
+    bluetoothAddressState = 1;
+    Serial.print(F("bluetooth address is: "));
+    Serial.println(bluetoothAddress);
+  } 
 
   /*for (servoPos = 0; servoPos <= 180; servoPos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
@@ -187,12 +164,12 @@ void loop() {
   
   // Process the bluetooth command queue, which is all the commands from our remote control.
   bluetoothProcess();
-  
+
   // Update the LED screen with our current state.
   bool connected = (bluetoothState == BLUETOOTH_CONNECTED);
   int upSecs = (millis() - startTime) / 1000;
   displayStatus(
-    connected ? F("CONNECTED") : F("DISCONNECTED"),
+    connected ? "CONNECTED" : "DISCONNECTED",
     "runtime: " + String(upSecs), 
     "cmd: " + String(command) + "/" + String(prevCommand) + "   v: " + String(velocity),
     "objective: " + String("KILL"));
@@ -201,7 +178,7 @@ void loop() {
   if (autoShutOff && (millis() > (startTime + AUTO_SHUTOFF_TIME))) {
     dead = true;
     motorStop();
-    displayMessage("Auto-Stop", "Stopped due to shutdown timer");
+    //displayMessage("Auto-Stop", "Stopped due to shutdown timer");
     delay(3000);
   }
 }
@@ -299,6 +276,99 @@ void motorStop() {
 
 /**** BLUETOOTH ****/
 
+/*
+ * Write one line out to the bluetooth in command mode.
+ */
+void writeToBluetoothCommand(const char * data) {
+  Serial.print(F("bluetooth sent: "));
+  Serial.println(data);
+  bluetooth.println(data);
+}
+
+/**
+ * Read one line of text from the bluetooth during command mode; all responses are separeated by a CR/LF.
+ */
+const char * readFromBluetoothCommand() {
+
+  // We will give a limited amount of time for the bluetooth to respond.
+  int startTime = millis();
+  int endTime = startTime + 10000;
+  
+  // Continue reading characters until we get to the end.
+  int curr = 0;
+  bool readComplete = false;
+  while (!readComplete && (curr < BLUETOOTH_READ_BUFFER) && (millis() < endTime)) {
+    if (bluetooth.available()) {
+      char c = bluetooth.read();
+      Serial.println("\nbluetooth read: " + String(c));
+      bluetoothReadbuffer[curr++] = c;
+      if (c == 10)
+        readComplete = true;
+    }
+  }
+
+  // If we did not read a full complete line, return the error signal.
+  if (!readComplete) {
+    bluetoothReadbuffer[0] = 0;
+  } else {
+    // cap off the CR/LF from the end of the response.
+    curr -= 2;
+    bluetoothReadbuffer[curr++] = 0; 
+  }
+
+  Serial.print(F("bluetooth read: "));
+  Serial.println(bluetoothReadbuffer);
+  return bluetoothReadbuffer;
+}
+
+/**
+ * Cycle the power on the bluetooth module, starting it back up in normal mode.
+ */
+void switchToBluetoothNormalMode() {
+  Serial.println(F("bluetooth normal mode starting..."));
+  
+  // power it down
+  digitalWrite(PIN_BLUETOOTH_POWER, LOW);
+  delay(500);
+
+  // power it up with the enable pin off.
+  digitalWrite(PIN_BLUETOOTH_ENABLE, LOW);
+  delay(500);
+  digitalWrite(PIN_BLUETOOTH_POWER, HIGH);
+  delay(750);
+
+  Serial.println(F("bluetooth normal mode started..."));
+}
+
+/**
+ * Cycle the power on the bluetooth module, starting it back up in command mode.
+ */
+void switchToBluetoothCommandMode() {
+  Serial.println(F("bluetooth command mode starting..."));
+  
+  // power it down
+  digitalWrite(PIN_BLUETOOTH_POWER, LOW);
+  delay(500);
+
+  // power it up with the enable pin on.
+  digitalWrite(PIN_BLUETOOTH_ENABLE, HIGH);
+  delay(500);
+  digitalWrite(PIN_BLUETOOTH_POWER, HIGH);
+  delay(750);
+  
+  Serial.println(F("bluetooth command mode started..."));
+}
+
+/**
+ * Turn the power off the bluetooth module.
+ */
+void switchToBluetoothOffMode() {
+  // power it down
+  digitalWrite(PIN_BLUETOOTH_POWER, LOW);
+  delay(100);
+}
+
+
 /**
  * 
  */
@@ -319,12 +389,12 @@ void bluetoothProcess() {
   prevCommand = command;
   command = bluetooth.read();
   if (command != 'S') {
-    Serial.println("cmd2new: " + String(command) + "/" + String(prevCommand));
+    //Serial.println("cmd2new: " + String(command) + "/" + String(prevCommand));
   }
   
   // Process the known commands. Note that we change pin mode only if new command is different from previous.  
   if (command != prevCommand) {
-    Serial.println("cmd2: " + String(command));
+    //Serial.println("cmd2: " + String(command));
     switch (command){
     case 'F': 
       motorForward();
@@ -363,11 +433,11 @@ void bluetoothProcess() {
       break;
     case 'U':  //Back ON
       //digitalWrite(pinbackLights, HIGH);
-      servoMotor.write(180.0);  
+      //servoMotor.write(180.0);  
       break;
     case 'u':  //Back OFF
       //digitalWrite(pinbackLights, LOW);
-      servoMotor.write(0.0);  
+      //servoMotor.write(0.0);  
       break;
     case 'D':  //Everything OFF
       digitalWrite(PIN_LED, LOW);
@@ -390,4 +460,66 @@ void bluetoothProcess() {
       }
     }
   }
+}
+
+/*** DISPLAY FUNCTIONS ***/
+
+/**
+ * Main output for status while in main sequence. This is called once per loop.
+ * TODO: can we get rid of strings? they are memory hogs.
+ */
+void displayStatus(String line1, String line2, String line3, String line4) {  
+  display.clear();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0,2);
+  display.println(line1);
+
+  // Fun little animation to prove that we are not locked up.
+  const int circleRadius = 5;
+  const int circleOffset = 0;
+  const int maxRight = (SCREEN_WIDTH - 1) - circleRadius;
+  const int maxLeft = 82;
+  int numFrames = maxRight - maxLeft;
+  int numFramesDouble = numFrames * 2;
+  int timePerFrame = 3000 / numFramesDouble;
+  int currentFrame = (millis() / timePerFrame) % numFramesDouble;
+  int circleCenterX = (currentFrame < numFrames) ? (maxRight - currentFrame) : (maxLeft + (currentFrame - numFrames));
+  display.drawCircle(circleCenterX, circleRadius + circleOffset, circleRadius, WHITE); 
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,16);
+  display.println(line2);
+  //display.drawLine(0, 20, SCREEN_WIDTH - 1, 20, WHITE); 
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,26);
+  display.println(line3);
+  //display.drawLine(0, 30, SCREEN_WIDTH - 1, 30, WHITE);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,36);
+  display.println(line4);
+  //display.drawLine(0, 40, SCREEN_WIDTH - 1, 40, WHITE);
+
+  int beginBlue = 46;
+  display.setCursor(0, beginBlue);
+  display.print("blue:");
+  display.setCursor(34, beginBlue);
+  display.println(bluetoothAddress);
+  
+  // Print the timestamp of when we built this code.
+  const int stampHeight = 10;
+  int beginStamp = 55;
+  display.drawLine(0, beginStamp, SCREEN_WIDTH - 1, beginStamp, WHITE); // top line
+  display.drawLine(0, beginStamp, 0, beginStamp + stampHeight, WHITE); // left line
+  display.drawLine(SCREEN_WIDTH - 1, beginStamp, SCREEN_WIDTH - 1, beginStamp + stampHeight, WHITE); // right line
+  display.setCursor(4, beginStamp + 2);
+  display.println(buildTimestamp);
+
+  display.update();
 }
