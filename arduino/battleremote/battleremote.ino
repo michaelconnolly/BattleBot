29b8af2d1d39797c773bc162893d812190e5fa45
mc <mc@madronahouse.com>
2020-02-25 22:53:46 -0800

    Knobs.

--- arduino/battleremote/battleremote.ino
+++ arduino/battleremote/battleremote.ino
@@ -19,6 +19,10 @@
 #define PIN_BUTTON_BLUE   5
 #define PIN_BUTTON_RED    6
 #define PIN_BUTTON_GREEN  7
+#define PIN_KNOB_BLUE  A3
+#define PIN_KNOB_RED  A2
+int knobBlueValue = 0;
+int knobRedValue = 0;
 
 // More Constants
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -107,6 +111,8 @@ void setup() {
   pinMode(PIN_BUTTON_BLUE, INPUT);
   pinMode(PIN_BUTTON_RED, INPUT);
   pinMode(PIN_BUTTON_GREEN, INPUT);
+  pinMode(PIN_KNOB_BLUE, INPUT);
+  pinMode(PIN_KNOB_RED, INPUT);
   
   // Init the OLED display.
   delay(1000);
@@ -207,6 +213,8 @@ void loop() {
   processButton(PIN_BUTTON_BLUE, '@');
   processButton(PIN_BUTTON_RED, '#');
   processButton(PIN_BUTTON_GREEN, '$');
+  processKnob(PIN_KNOB_BLUE, knobBlueValue);
+  processKnob(PIN_KNOB_RED, knobRedValue);
     
   // Update the LED screen with our current state.
   bool connected = (bluetoothState == BLUETOOTH_CONNECTED);
@@ -218,23 +226,42 @@ void loop() {
     "dude: " + String("yo"));
 }
 
+
 void processButton(int buttonId, char commandToSend) {
 
   int buttonState = digitalRead(buttonId);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    //digitalWrite(ledPin, HIGH);
     Serial.print("command to send: ");
     Serial.println(commandToSend);
     bluetooth.write(commandToSend);
+  } 
+}
+
+
+void processKnob(int knobId, int &knobValueOld) {
+
+  int knobValue = analogRead(knobId);
+
+  // TODO: a knob should make something going left and right.
+  // One model is that the span of the knob's rotation covers the same span
+  // that the thing is that we are moving.
+  // Another model is that anything left of center means keep going to the left,
+  // and a particular speed.  Moving the knob into it's center position means
+  // to stop moving.
+
+  // If the value is different from the last value we knew about, 
+  // we should probably tell the robot that the value changed.
+  if (knobValue != knobValueOld) {
+    
+    Serial.print("knobId: ");
+    Serial.print(knobId);
+    Serial.print(", knobValue: ");
+    Serial.println(knobValue);
+
+    knobValueOld = knobValue;
   }
-//  } else {
-//    // turn LED off:
-//    //digitalWrite(ledPin, LOW);
-//  }
-//  
 }
 
 
