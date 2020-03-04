37d4c43e0dc7f0e90523098888dc0db3d811eb64
mc <mc@madronahouse.com>
2020-03-03 23:41:34 -0800

    Update battleremote.ino

--- arduino/battleremote/battleremote.ino
+++ arduino/battleremote/battleremote.ino
@@ -15,16 +15,6 @@
 #define PIN_JOYSTICK_X     A6
 #define PIN_JOYSTICK_Y     A7
 
-//// Constants: knobs and buttons.
-//#define PIN_BUTTON_YELLOW 4
-//#define PIN_BUTTON_BLUE   5
-//#define PIN_BUTTON_RED    6
-//#define PIN_BUTTON_GREEN  7
-//#define PIN_KNOB_BLUE  A3
-//#define PIN_KNOB_RED  A2
-//int knobBlueValue = 0;
-//int knobRedValue = 0;
-
 // More Constants
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -68,9 +58,7 @@ unsigned long bluetoothPulseLastRecvTime = 0;
 char lastCommand = 0;
 
 // Remote class that implements all business logic.
-//void DebugMessage(char* message);
 custom_remote* remote = new custom_remote(&bluetooth);
-//string my_string = string("foo");
 
 
 /**
@@ -112,14 +100,6 @@ void setup() {
   bluetoothPulseLastRecvTime = 0;
   bluetooth.begin(38400);
   Serial.println(F("setup: Bluetooth complete..."));
-
-// // Init: knobs and buttons.
-//  pinMode(PIN_BUTTON_YELLOW, INPUT);
-//  pinMode(PIN_BUTTON_BLUE, INPUT);
-//  pinMode(PIN_BUTTON_RED, INPUT);
-//  pinMode(PIN_BUTTON_GREEN, INPUT);
-//  pinMode(PIN_KNOB_BLUE, INPUT);
-//  pinMode(PIN_KNOB_RED, INPUT);
 
   // Custom Remote logic.
   //remote = new custom_remote(bluetooth);
@@ -233,58 +213,9 @@ void loop() {
     "dude: " + String("yo"));
 
 
-// // Loop: buttons and knobs.
-//  processButton(PIN_BUTTON_YELLOW, '!');
-//  processButton(PIN_BUTTON_BLUE, '@');
-//  processButton(PIN_BUTTON_RED, '#');
-//  processButton(PIN_BUTTON_GREEN, '$');
-//  processKnob(PIN_KNOB_BLUE, knobBlueValue);
-//  processKnob(PIN_KNOB_RED, knobRedValue);
+  // Loop: custom remote class.
   remote->loop();
-
- //Serial.println(F("test 1!"));
-
-   // Serial.println(F("test 2!"));
 }
-
-//void processButton(int buttonId, char commandToSend) {
-//
-//  int buttonState = digitalRead(buttonId);
-//
-//  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-//  if (buttonState == HIGH) {
-//    Serial.print(F("command to send: "));
-//    Serial.println(commandToSend);
-//    bluetooth.write(commandToSend);
-//  } 
-//}
-//
-//
-//void processKnob(int knobId, int &knobValueOld) {
-//
-//  int knobValue = analogRead(knobId);
-//
-//  // TODO: a knob should make something going left and right.
-//  // One model is that the span of the knob's rotation covers the same span
-//  // that the thing is that we are moving.
-//  // Another model is that anything left of center means keep going to the left,
-//  // and a particular speed.  Moving the knob into it's center position means
-//  // to stop moving.
-//
-//  // If the value is different from the last value we knew about, 
-//  // we should probably tell the robot that the value changed.
-//  if (knobValue != knobValueOld) {
-//    
-//    Serial.print(F("knobId: "));
-//    Serial.print(knobId);
-//    Serial.print(F(", knobValue: "));
-//    Serial.println(knobValue);
-//
-//    knobValueOld = knobValue;
-//  }
-//}
-
-
 
 /**
  * 
